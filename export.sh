e5d6ac95621c3599a9b9e6d9969b63e9bdb34dd4
Il Jae Lee <agiantwhale@gmail.com>
2018-05-04 13:55:35 -0400

    Housekeeping

--- export.sh
+++ export.sh
@@ -8,9 +8,15 @@ mkdir -p outputs/images
 
 NUM_MAZES=10
 
+if [[ ! -z "${ACC}" ]]; then
+	{
+		${ACC} ./content/static_goal_random_spawn.acs content/static_goal_random_spawn.o
+	} &> /dev/null
+fi
+
 for SIZE in $(seq -s ' ' 3 2 11); do
 	PREFIX="./outputs/sources/$SIZE"
 	python maze.py $PREFIX -r $SIZE -c $SIZE -n $NUM_MAZES
 	python wad.py $PREFIX "outputs/$SIZE.wad" -b content/static_goal_random_spawn.o
 done
-echo "Finished."
+echo "Success."
