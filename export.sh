d9eaa1c247eeae26423e500318c592f35788df6c
Il Jae Lee <agiantwhale@gmail.com>
2018-05-07 21:31:02 -0700

    Updated number of maps

--- export.sh
+++ export.sh
@@ -6,15 +6,15 @@ mkdir -p outputs
 mkdir -p outputs/sources
 mkdir -p outputs/images
 
-NUM_MAZES=10
+NUM_MAZES=1000
 
 if [[ ! -z "${ACC}" ]]; then
 	{
-		${ACC} ./content/static_goal_random_spawn.acs content/static_goal_random_spawn.o
+		${ACC} ./content/static_goal_random_spawn.acs ./content/static_goal_random_spawn.o
 	} &> /dev/null
 fi
 
-for SIZE in $(seq -s ' ' 3 2 11); do
+for SIZE in $(seq -s ' ' 7 2 11); do
 	PREFIX="./outputs/sources/$SIZE"
 	python maze.py $PREFIX -r $SIZE -c $SIZE -n $NUM_MAZES
 	python wad.py $PREFIX "outputs/$SIZE.wad" -b content/static_goal_random_spawn.o
